6283f95d42a60e879967ec2de3415918dd92a33f
Walter Capa <walterc316@gmail.com>
2020-03-14 19:31:22 -0500

    Changed the response util method so it changes and uses the bottle response instead of sending the status as part of the data

--- src/utils/response.py
+++ src/utils/response.py
@@ -4,10 +4,10 @@ from src.utils import status as status_codes
 
 
 def response(data, status=status_codes.HTTP_200_OK):
+    bottle.response.headers['Content-Type'] = 'application/json'
+    bottle.response.status = status
+
     if status >= status_codes.HTTP_400_BAD_REQUEST:
         bottle.abort(status, data)
 
-    return {
-        'data': data,
-        'status_code': status
-    }
+    return data
